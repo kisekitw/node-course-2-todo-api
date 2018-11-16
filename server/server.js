c19d55ec9d60ea349774d6aaf76fd72eca53d2a4
kisekitw <kisekitw@gmail.com>
2018-11-16 14:43:34 +0800

    login

--- server/server.js
+++ server/server.js
@@ -104,7 +104,6 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
-// POST /users
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -122,6 +121,20 @@ app.post('/users', (req, res) => {
     });
 
 });
+
+app.post('/users/login', (req, res) => {
+   var body = _.pick(req.body, ['email', 'password']);
+
+   User.findByCredentials(body.email, body.password)
+    .then((user) => {
+        return user.generateAuthToken()
+                .then((token) => {
+                    res.header('x-auth', token).send(user);
+                });
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
 
 app.get('/users/me', authenticate, (req, res) => {
     // Move to authenticate
